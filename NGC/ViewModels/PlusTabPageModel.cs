aab7d8b20b197997e5270608530fa7b56c558144
Vaibhav Khanna <y2kvaibhav@gmail.com>
2018-11-22 14:25:53 +0530

    Changes

--- NGC/ViewModels/PlusTabPageModel.cs
+++ NGC/ViewModels/PlusTabPageModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using Xamarin.Forms;
+
 namespace NGC.ViewModels
 {
     public class PlusTabPageModel : BaseViewModel
@@ -6,5 +8,17 @@ namespace NGC.ViewModels
         public PlusTabPageModel()
         {
         }
+
+
+        public Command MapCommand => new Command(async() =>
+        {
+            await CoreMethods.PushPageModel<MapPageModel>();
+        });
+
+        public Command LogOutCommand => new Command(async() =>
+        {
+            await CoreMethods.DisplayAlert("Déconnexion", "Êtes-vous sûr de vouloir vous déconnecter ?", "Oui", "Non");
+        });
+
     }
 }
